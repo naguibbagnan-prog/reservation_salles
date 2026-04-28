1b91249d8d1cffb261867b00b31c65336fa1a73d
BAGNAN Abdel-Naguib <naguibbagnan@gmail.com>
2026-04-29 00:19:40 +0200

    Bloc 6 : Ajout du point d'entree final avec interface graphique

---
+++ main_final.py
@@ -0,0 +1,19 @@
+"""
+Systeme de Reservation de Salles - Universite de Parakou
+Version finale avec interface graphique.
+
+Auteurs : ACODODJA Melaine, BAGNAN Abdel-Naguib
+Encadre par : Dr MOUSSE
+"""
+
+from ui.app_gui import lancer
+
+
+def main():
+    print("Demarrage du systeme de reservation...")
+    print("Universite de Parakou - Projet 7")
+    lancer()
+
+
+if __name__ == "__main__":
+    main()
